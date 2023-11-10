fff7e2d1677bd96bb9c2d6709d1ed377f5f13e69
SATHIYAPRIYAN7 <sathiyapriyan7@gmail.com>
2023-11-10 12:08:46 +0530

    sec

--- src/components/Bankofreact.js
+++ src/components/Bankofreact.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 
 
 function Bankofreact() {
@@ -7,21 +7,51 @@ function Bankofreact() {
   
  
    const [home,setHome]=useState(1000);
-   const [down,setDown]=useState(0);
-   const [loan,setLoan]=useState(0);
+   const [down,setDown]=useState(200);
+   const [loan,setLoan]=useState(800);
    const [int,setInt]=useState(2);
    const[month,setMonth]=useState(0)
 
-   useEffect(()=>{
-    console.log(home)
-    setDown(home*0.2);
-   },[home])
+  //  useEffect(()=>{
+  //   console.log(home)
+  //   setDown(home*0.2);
+  //  },[home])
 
-   useEffect(()=>{
-    console.log()
-    setLoan(home-down)
-   },[down])
+  //  useEffect(()=>{
+  //   console.log()
+  //   setLoan(home-down)
+   
+  //  },[down])
 
+  useEffect(() => {
+    setLoan(home - down);
+  }, [down]);
+  
+  useEffect(() => {
+    setDown(home - loan);
+  }, [loan]);
+
+   
+
+   const data = [
+    { name: 'Home Value', value: home },
+    { name: 'Loan Amount', value: loan },
+  ];
+
+     const COLORS = ['#0088FE', '#00C49F'];
+
+     const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
   
 
   return (
@@ -30,9 +60,11 @@ function Bankofreact() {
             <h1 style={{marginLeft:"20px",fontSize:"24px",fontWeight:"bold",color:"white"}}>Bank of React</h1>
          </div>
 
-         <div style={{margin:"20px 20px",maxWidth:"800px"}}>
+         <div style={{margin:"20px 20px", display:'flex',width:"100%",justifyContent:"space-between",alignItems:"center"}}>
 
-          <div >
+          <div style={{width:"50%"}}>
+
+          <div>
             <h1>Home Value</h1>
             <h1>$ {home}</h1>
             <input type='range' min="1000" max="10000" step="100" value={home} style={{width:'100%'}} onChange={(e)=>setHome(e.target.value) }></input>
@@ -55,10 +87,10 @@ function Bankofreact() {
          <div>
             <h1>Loan Amount</h1>
             <h1>$ {loan}</h1>
-            <input type='range' min="0" max="10000" step="100" style={{width:'100%'}} value={loan} onChange={(e)=> setLoan(e.target.value)}></input>
+            <input type='range' min="0" max={home} step="100" style={{width:'100%'}} value={loan} onChange={(e)=> setLoan(e.target.value)}></input>
             <div style={{display:"flex",justifyContent:"space-between"}}>
               <p style={{fontSize:"14px"}}>$0</p>
-              <p style={{fontSize:"14px"}}>$10000</p>
+              <p style={{fontSize:"14px"}}>${home}</p>
             </div>
          </div>
 
@@ -84,13 +116,30 @@ function Bankofreact() {
 </select>
          </div>
 
-
          </div>
+        
 
-         <div>
-           
+         <div style={{width:"50%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+          
+          <h1 style={{fontSize:"30px",fontWeight:"bold"}}>Monthly Payment: $ {loan*0.02}</h1>
+         <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
           </div>
-
+          </div>
         
     </div>
   )
